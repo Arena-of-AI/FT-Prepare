4684f31a50807192478bb539533c664dc45cbc03
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-12 16:16:06 +0800

    Update Training.py

--- Training.py
+++ Training.py
@@ -56,27 +56,27 @@ def main():
 
             # 运行 CLI 命令
             command = f"OPENAI_API_KEY={api_key} openai tools fine_tunes.prepare_data -f {file_path}"
-            error = run_command(command)
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True)
+
+            # 显示终端输出和等待用户回答
+            with st.expander("Terminal Output"):
+                output_lines = []
+                for line in process.stdout:
+                    output_lines.append(line.strip())
+                    st.text(line.strip())
+
+                # 在终端输出中寻找需要用户回答的问题
+                question_indices = [i for i, line in enumerate(output_lines) if line.startswith("[Recommended]") or line.startswith("[Required]")]
+                for index in question_indices:
+                    question = output_lines[index]
+                    default_answer = "Y" if question.endswith("[Y/n]:") else ""
+                    answer = st.text_input(question, default_answer)
+                    process.stdin.write(answer + "\n")
+                    process.stdin.flush()
+
+            _, error = process.communicate()
 
             if not error:
                 # 解析 CLI 输出并获取生成的 JSONL 文件名
                 jsonl_filename = ""
-                with os.scandir(downloads_folder) as entries:
-                    for entry in entries:
-                        if entry.name.endswith(".jsonl"):
-                            jsonl_filename = entry.name
-                            break
-
-                if jsonl_filename:
-                    # 下载生成的 JSONL 文件
-                    download_file(os.path.join(downloads_folder, jsonl_filename))
-                else:
-                    st.warning("Failed to find JSONL file")
-            else:
-                st.warning("An error occurred during data preparation")
-                st.error(error)
-        else:
-            st.warning("Please upload an Excel file and enter your OpenAI API Key")
-
-if __name__ == "__main__":
-    main()
+                with os.scandir(downloads_folder
