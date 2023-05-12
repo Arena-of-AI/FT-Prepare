c0d6c354cf3b11c7fb88dbad7a4d7c01ad6f74ac
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-12 15:52:02 +0800

    Update Training.py

--- Training.py
+++ Training.py
@@ -3,9 +3,12 @@ import streamlit as st
 import subprocess
 
 def run_command(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    return output.decode(), error.decode()
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True)
+    with st.beta_expander("Terminal Output"):
+        for line in process.stdout:
+            st.text(line.strip())
+    _, error = process.communicate()
+    return error
 
 def create_temp_folder():
     # 创建 "temp" 文件夹
@@ -48,26 +51,25 @@ def main():
 
             # 运行 CLI 命令
             command = f"openai tools fine_tunes.prepare_data -f {file_path}"
-            output, error = run_command(command)
+            error = run_command(command)
 
-            # 解析 CLI 输出并获取生成的 JSONL 文件名
-            output_lines = output.split("\n")
-            jsonl_filename = ""
-            for line in output_lines:
-                if line.startswith("Wrote modified files to"):
-                    jsonl_filename = line.split("`")[1]
-                    break
+            if not error:
+                # 解析 CLI 输出并获取生成的 JSONL 文件名
+                jsonl_filename = ""
+                with os.scandir(downloads_folder) as entries:
+                    for entry in entries:
+                        if entry.name.endswith(".jsonl"):
+                            jsonl_filename = entry.name
+                            break
 
-            if jsonl_filename:
-                # 将生成的 JSONL 文件移动到 "downloads" 文件夹
-                output_file = os.path.join(os.getcwd(), jsonl_filename)
-                output_file_destination = os.path.join(downloads_folder, "prepared_data.jsonl")
-                os.rename(output_file, output_file_destination)
-
-                # 下载生成的 JSONL 文件
-                download_file(output_file_destination)
+                if jsonl_filename:
+                    # 下载生成的 JSONL 文件
+                    download_file(os.path.join(downloads_folder, jsonl_filename))
+                else:
+                    st.warning("Failed to find JSONL file")
             else:
-                st.warning("Failed to generate JSONL file")
+                st.warning("An error occurred during data preparation")
+                st.error(error)
         else:
             st.warning("Please upload an Excel file")
 
