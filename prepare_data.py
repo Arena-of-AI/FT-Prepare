f563f5dad77f8a683f2e98b5cb2fd0ee7247e582
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-15 12:31:12 +0800

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -1,37 +1,62 @@
 import os
 import streamlit as st
 import subprocess
-import pexpect
 
-def run_command(command, answers):
-    process = pexpect.spawn(command)
-    process.expect("^\[.*\]$", timeout=None)  # 等待命令提示符出现
-    for answer in answers:
-        process.sendline(answer)
-        process.expect("^\[.*\]$", timeout=None)  # 等待下一个问题出现
-    output = process.read().decode()
-    return output
+def run_command(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True)
+    with st.expander("Terminal Output"):
+        for line in process.stdout:
+            st.text(line.strip())
+    _, error = process.communicate()
+    return error
 
-# 其他代码保持不变
+def create_temp_folder():
+    # 创建 "temp" 文件夹
+    if not os.path.exists("temp"):
+        os.makedirs("temp")
+    return "temp"
+
+def create_downloads_folder():
+    # 创建 "downloads" 文件夹
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
+    return "downloads"
 
 def main():
-    # ...
+    st.title("Data Preparation Tool")
+
+    # 输入 OpenAI API 密钥
+    api_key = st.text_input("Enter OpenAI API Key")
+    if not api_key:
+        st.warning("Please enter your OpenAI API Key")
+
+    temp_folder = create_temp_folder()
+    downloads_folder = create_downloads_folder()
+
+    # 上传 Excel 文件
+    uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
+    if uploaded_file is not None:
+        # 显示上传的文件名
+        st.write("Uploaded file:", uploaded_file.name)
 
     # 准备数据并生成 JSONL 文件
     if st.button("Prepare Data"):
         if uploaded_file is not None and api_key:
-            # ...
+            # 保存上传的 Excel 文件
+            file_path = os.path.join(temp_folder, uploaded_file.name)
+            with open(file_path, "wb") as f:
+                f.write(uploaded_file.getbuffer())
 
             # 运行 CLI 命令
             command = f"OPENAI_API_KEY={api_key} openai tools fine_tunes.prepare_data -f {file_path}"
             
             # 自动回答所有问题
-            answers = ["y"] * 5  # 回答5个问题都是 "y"
-            output = run_command(command, answers)
+            input_answers = "y\n" * 5  # 回答5个问题都是 "y"
+            command_with_answers = f"echo '{input_answers}' | {command}"
+            
+            error = run_command(command_with_answers)
 
-            if not output:
-                st.warning("An error occurred during data preparation")
-            else:
+            if not error:
                 # 查找生成的 JSONL 文件
                 jsonl_filename = None
                 for filename in os.listdir(downloads_folder):
@@ -45,10 +70,11 @@ def main():
                     st.markdown(f"### [Download Prepared Data JSONL]({download_link})")
                 else:
                     st.warning("Failed to find JSONL file")
+            else:
+                st.warning("An error occurred during data preparation")
+                st.error(error)
         else:
             st.warning("Please upload an Excel file and enter your OpenAI API Key")
-
-# 其他代码保持不变
 
 if __name__ == "__main__":
     main()
