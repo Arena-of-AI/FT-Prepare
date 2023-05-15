f3faeccddfac2200b691f6ae5cb438e548a7501c
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-15 10:53:23 +0800

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,5 +1,31 @@
+import os
+import streamlit as st
+import subprocess
+
+def run_command(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    return output.decode(), error.decode()
+
+def create_temp_folder():
+    # 创建 "temp" 文件夹
+    if not os.path.exists("temp"):
+        os.makedirs("temp")
+    return "temp"
+
+def create_downloads_folder():
+    # 创建 "downloads" 文件夹
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
+    return "downloads"
+
 def main():
     st.title("Data Preparation Tool")
+
+    # 输入 OpenAI API 密钥
+    api_key = st.text_input("Enter OpenAI API Key")
+    if not api_key:
+        st.warning("Please enter your OpenAI API Key")
 
     temp_folder = create_temp_folder()
     downloads_folder = create_downloads_folder()
@@ -20,37 +46,21 @@ def main():
 
             # 运行 CLI 命令
             command = f"OPENAI_API_KEY={api_key} openai tools fine_tunes.prepare_data -f {file_path}"
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True)
-
-            # 显示终端输出和等待用户回答
-            with st.expander("Terminal Output"):
-                output_lines = []
-                for line in process.stdout:
-                    output_lines.append(line.strip())
-                    st.text(line.strip())
-
-                # 在终端输出中寻找需要用户回答的问题
-                question_indices = [i for i, line in enumerate(output_lines) if line.startswith("[Recommended]") or line.startswith("[Required]")]
-                for index in question_indices:
-                    question = output_lines[index]
-                    default_answer = "Y" if question.endswith("[Y/n]:") else ""
-                    answer = st.text_input(question, default_answer)
-                    process.stdin.write(answer + "\n")
-                    process.stdin.flush()
-
-            _, error = process.communicate()
+            output, error = run_command(command)
 
             if not error:
-                # 查找生成的 JSONL 文件
-                jsonl_filename = None
-                for filename in os.listdir(downloads_folder):
-                    if filename.endswith(".jsonl"):
-                        jsonl_filename = filename
+                # 解析 CLI 输出并获取生成的 JSONL 文件名
+                jsonl_filename = ""
+                lines = output.splitlines()
+                for line in lines:
+                    if line.startswith("Wrote modified files to "):
+                        jsonl_filename = line.split()[-1]
                         break
 
                 if jsonl_filename:
                     # 下载生成的 JSONL 文件
-                    download_file(os.path.join(downloads_folder, jsonl_filename))
+                    download_link = f"./downloads/{jsonl_filename}"
+                    st.markdown(f"### [Download Prepared Data JSONL]({download_link})")
                 else:
                     st.warning("Failed to find JSONL file")
             else:
@@ -58,3 +68,6 @@ def main():
                 st.error(error)
         else:
             st.warning("Please upload an Excel file and enter your OpenAI API Key")
+
+if __name__ == "__main__":
+    main()
